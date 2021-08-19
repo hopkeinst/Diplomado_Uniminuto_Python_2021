1d1d89a64da828ef80c14101f9337fe06e177c37
Jorge Saul <hopkeinst@gmail.com>
2021-08-19 16:26:04 -0500

    Trabajo 2 listas

---
+++ 041_Listas_2/041_DosListas.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+sistema_operativo = sys.platform
+if sistema_operativo.startswith('win'):
+	os.system("cls")
+else:
+	os.system("clear")
+
+print("LISTAS - TRABAJO CON 2 LISTAS")
+print("Se van a trabajar 2 listas para guardar datos de productos y cantidades.")
+
+print("\nVas a ingresar los productos a medida que se le solicite.\nSi no desea ingresar más productos ingrese 'NO' cuando se le solicite el producto.\n")
+
+productos = []
+p = ""
+while p != "no":
+	p = input("-> Producto: ")
+	if p.lower() != "no":
+		productos.append(p)
+
+cantidades = []
+print("\nAhora vas a ingresar las cantidades para cada producto")
+for i in productos:
+	cnt = None
+	while cnt == None:
+		try:
+			str_input = input("Ingrese la cantidad para el producto {:s}: ".format(i))
+			cnt = int(str_input)
+		except ValueError:
+			try:
+				cnt = float(str_input)
+			except ValueError:
+				print("-- ERROR -- Ingresaste un valor no numérico (entero o con decimales).\n\tInténtelo de nuevo.\n")
+				cnt = None
+	cantidades.append(cnt)
+
+print()
+for i in range(len(productos)):
+	print("El producto {:s} tiene {} existencias".format(productos[i], cantidades[i]))
+
+print()
